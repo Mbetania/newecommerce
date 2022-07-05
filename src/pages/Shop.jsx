6d6b61e67bca642c94c566c0afab02dbfa0af5e5
brunomartin <bruno.pedraza@conexa.ai>
2022-07-04 23:02:11 -0300

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,18 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import Count from '../components/unit/Count'
-import ItemListContainer from './ItemListContainer'
-
-
-export const Shop = ({ onAdd }) => {
-    return (
-        <>
-            <ItemListContainer />
-            <Count stock={5} initial={1}/>
-
-        </>
-    )
-=======
 import { NavBar } from '../components/shop/NavBar'
 import Count from '../components/unit/Calculator'
 import ItemListContainer from './ItemListContainer'
@@ -26,5 +12,4 @@ return (
 
     </>
 )
->>>>>>> calculator
 }
